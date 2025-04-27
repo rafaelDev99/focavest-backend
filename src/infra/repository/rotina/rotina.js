1c490d129b38f718c8c855c8c44330edf1d957a6
Rafael A <rafaelcardosocatuti@gmail.com>
2025-04-27 14:24:23 -0300

    rotina repository

--- src/infra/repository/rotina/rotina.js
+++ src/infra/repository/rotina/rotina.js
@@ -1,12 +1,39 @@
 const sql = require('../../../config/db')
 
 class RotinaRepository{
-    async getRotinasByUsuario(email){
-        const result = await sql`
-          SELECT id, nome, email, tipo_usuario 
-          FROM usuario 
-          WHERE email = ${email}
-        `;
-        return result[0];
-      }
+  async getRotinasByUsuario(id){
+    const result = await sql`
+      SELECT id, nome, descricao, tempo_total, materia, topico, data
+      FROM rotina
+      WHERE usuario_id = ${id}
+    `;
+    return result;
+  }
+
+  async getRotinaById(id){
+    const result = await sql`
+      SELECT id, nome, descricao, tempo_total, materia, topico, data
+      FROM rotina
+      WHERE id = ${id}
+    `;
+    return result;
+  }
+
+  async createRotina(rotina){
+    const {
+      nome,
+      descricao,
+      tempo_total,
+      materia,
+      topico,
+      data,
+      usuario_id
+    } = rotina;
+    const result = await sql`
+      INSERT INTO rotina (nome, descricao, tempo_total, materia, topico, data, usuario_id)
+      VALUES (${nome}, ${descricao}, ${tempo_total}, ${materia}, ${topico}, ${data}, ${usuario_id})
+      RETURNING id, nome, descricao, tempo_total, materia, topico, data
+    `;
+    return result;
+  }
 }
